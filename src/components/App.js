1445ddcd30d063a9bd17e8b9609890d852cf845c
Shashank7016 <bryanhovak@gmail.com>
2024-06-10 02:21:15 +1000

    making changes in editor to show hints and dowload code locally

--- src/components/App.js
+++ src/components/App.js
@@ -1,62 +1,92 @@
-import React,{useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Layout, Menu, Space } from 'antd';
 import Editor from './Editor';
 import useLocalStorage from '../hooks/useLocalStorage';
+import '../App.css';
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
-  const [html,sethtml]=useLocalStorage('html','')
-  const [css,setcss]=useLocalStorage('css','')
-  const [js,setjs]=useLocalStorage('js','')
-  const [srcdoc,setsrcdoc]=useState('')
+  const [html, setHtml] = useLocalStorage('html', '');
+  const [css, setCss] = useLocalStorage('css', '');
+  const [js, setJs] = useLocalStorage('js', '');
+  const [srcDoc, setSrcDoc] = useState('');
 
   useEffect(() => {
-    const time=setTimeout(()=>{
-      setsrcdoc(`
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
         <html>
-         <body>${html}</body>
-         <style>${css}</style>
-         <script>${js}</script>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
         </html>
-      `)
-    },250)
-    return () => clearTimeout(time)
-  },[html,css,js])
-  
-  return (<>
-    <div className="panel panel-top">
-      <Editor
-      language="xml" 
-      displayName="HTML" 
-      value={html} 
-      onChange={sethtml}
-      />
-      <Editor
-      language="css" 
-      displayName="CSS" 
-      value={css} 
-      onChange={setcss}
-      />
-      <Editor
-      language="javascript" 
-      displayName="JS" 
-      value={js} 
-      onChange={setjs}
-      />
-    </div>
-    <div className="panel-bottom">   
-      <h1>Output:</h1>
-      <iframe 
-      srcdoc={srcdoc}
-      title="output" 
-      sandbox="allow-scripts" 
-      frameBorder="0" 
-      width="100%"
-      height="100%">
-       </iframe>
-    </div>
+      `);
+    }, 250);
+    return () => clearTimeout(timeout);
+  }, [html, css, js]);
 
-  </>
+  const downloadFile = (filename, content) => {
+    const element = document.createElement('a');
+    const file = new Blob([content], { type: 'text/plain' });
+    element.href = URL.createObjectURL(file);
+    element.download = filename;
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  };
 
-  )
+  return (
+    <Layout className="layout">
+      <Header>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1">Code Editor</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <div className="site-layout-content">
+          <Space direction="vertical" style={{ width: '100%' }}>
+            <div className="panel panel-top">
+              <Editor
+                language="xml"
+                displayName="HTML"
+                value={html}
+                onChange={setHtml}
+                onDownload={() => downloadFile('index.html', html)}
+              />
+              <Editor
+                language="css"
+                displayName="CSS"
+                value={css}
+                onChange={setCss}
+                onDownload={() => downloadFile('styles.css', css)}
+              />
+              <Editor
+                language="javascript"
+                displayName="JS"
+                value={js}
+                onChange={setJs}
+                onDownload={() => downloadFile('script.js', js)}
+              />
+            </div>
+            <div className="panel-bottom">
+              <h1>Output:</h1>
+              <iframe
+                srcDoc={srcDoc}
+                title="output"
+                sandbox="allow-scripts"
+                frameBorder="0"
+                width="100%"
+                height="100%"
+              ></iframe>
+            </div>
+          </Space>
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Code Editor ©2024 Created by YourName
+      </Footer>
+    </Layout>
+  );
 }
 
 export default App;
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,47 +1,77 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import 'codemirror/lib/codemirror';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
 import 'codemirror/mode/xml/xml';
 import 'codemirror/mode/css/css';
 import 'codemirror/mode/javascript/javascript';
+import 'codemirror/addon/hint/show-hint.css';
+import 'codemirror/addon/hint/show-hint';
+import 'codemirror/addon/hint/xml-hint';
+import 'codemirror/addon/hint/html-hint';
+import 'codemirror/addon/hint/css-hint';
+import 'codemirror/addon/hint/javascript-hint';
+import 'codemirror/addon/fold/foldgutter.css';
+import 'codemirror/addon/fold/foldgutter';
+import 'codemirror/addon/fold/brace-fold';
+import 'codemirror/addon/edit/closetag';
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/selection/active-line';
 import { Controlled as ControlledEditor } from 'react-codemirror2';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCompressAlt,faExpandAlt } from '@fortawesome/free-solid-svg-icons'
-export default function Editor(props){
-    const
-    {
-        language,
-        displayName,
-        value,
-        onChange
-    }=props
-    const[open,setopen]=useState(true)
-    function handleChange(editor,data,value){
-        onChange(value)
-    }   
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCompressAlt, faExpandAlt, faDownload } from '@fortawesome/free-solid-svg-icons';
 
-    return(
-        <div className={`code-container ${open ? '': 'collapsed'}`}>
-            <div className="code-Title">
-                {displayName}
-                <button
-                className="expcollbtn"
-                 onClick={() => setopen(prevopen => !prevopen)}>
-                    <FontAwesomeIcon icon={ open?faCompressAlt:faExpandAlt }/>
-                </button>
-            </div>
-            <ControlledEditor
-             onBeforeChange={handleChange}
-             value={value}
-             className="code-mirror-wrapper"
-             options={{
-                 lineWrapping:true,
-                 lint:true,
-                 mode:language,
-                 theme:'material',
-                 lineNumbers:true
-             }}
-             ></ControlledEditor>
+export default function Editor(props) {
+  const { language, displayName, value, onChange, onDownload } = props;
+  const [open, setOpen] = useState(true);
+
+  function handleChange(editor, data, value) {
+    onChange(value);
+  }
+
+  function handleEditorDidMount(editor) {
+    editor.setOption('extraKeys', {
+      'Ctrl-Space': 'autocomplete',
+    });
+    editor.on('inputRead', function onInputRead(cm, event) {
+      if (!cm.state.completionActive && event.text[0].match(/[.\w@({]/)) {
+        cm.showHint({ completeSingle: false });
+      }
+    });
+  }
+
+  return (
+    <div className={`code-container ${open ? '' : 'collapsed'}`}>
+      <div className="code-title">
+        {displayName}
+        <div>
+          <button className="expcollbtn" onClick={() => setOpen(prevOpen => !prevOpen)}>
+            <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+          </button>
+          <button className="expcollbtn" onClick={onDownload}>
+            <FontAwesomeIcon icon={faDownload} />
+          </button>
         </div>
-    )
+      </div>
+      <ControlledEditor
+        editorDidMount={handleEditorDidMount}
+        onBeforeChange={handleChange}
+        value={value}
+        className="code-mirror-wrapper"
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          theme: 'material',
+          lineNumbers: true,
+          extraKeys: { 'Ctrl-Space': 'autocomplete' },
+          foldGutter: true,
+          gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter'],
+          autoCloseTags: true,
+          autoCloseBrackets: true,
+          styleActiveLine: false,
+        }}
+      />
+    </div>
+  );
 }
